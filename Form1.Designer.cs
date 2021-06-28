e73af2c48a5702226703b491297d13556bef525b
SnickersZA <snickers.projects@gmail.com>
2021-06-28 20:29:04 +0200

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace IW4x_Server_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonOpenServerConfig = new System.Windows.Forms.Button();
+            this.buttonOpenLobbyConfig = new System.Windows.Forms.Button();
+            this.textBoxNetport = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxPlaylist = new System.Windows.Forms.ComboBox();
+            this.comboBoxLAN = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxMod = new System.Windows.Forms.ComboBox();
+            this.buttonDedicatedServer = new System.Windows.Forms.Button();
+            this.buttonPartyServer = new System.Windows.Forms.Button();
+            this.buttonMultiplayer = new System.Windows.Forms.Button();
+            this.buttonSingleplayer = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.buttonOpenServerConfig);
+            this.panel1.Controls.Add(this.buttonOpenLobbyConfig);
+            this.panel1.Controls.Add(this.textBoxNetport);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.comboBoxPlaylist);
+            this.panel1.Controls.Add(this.comboBoxLAN);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBoxMod);
+            this.panel1.Controls.Add(this.buttonDedicatedServer);
+            this.panel1.Controls.Add(this.buttonPartyServer);
+            this.panel1.Controls.Add(this.buttonMultiplayer);
+            this.panel1.Controls.Add(this.buttonSingleplayer);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("BankGothic Md BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(567, 327);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // buttonOpenServerConfig
+            // 
+            this.buttonOpenServerConfig.BackColor = System.Drawing.Color.Black;
+            this.buttonOpenServerConfig.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.buttonOpenServerConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpenServerConfig.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonOpenServerConfig.Location = new System.Drawing.Point(11, 241);
+            this.buttonOpenServerConfig.Name = "buttonOpenServerConfig";
+            this.buttonOpenServerConfig.Size = new System.Drawing.Size(254, 26);
+            this.buttonOpenServerConfig.TabIndex = 21;
+            this.buttonOpenServerConfig.Text = "OPEN SERVER CONFIG";
+            this.buttonOpenServerConfig.UseVisualStyleBackColor = false;
+            this.buttonOpenServerConfig.Click += new System.EventHandler(this.buttonOpenServerConfig_Click);
+            // 
+            // buttonOpenLobbyConfig
+            // 
+            this.buttonOpenLobbyConfig.BackColor = System.Drawing.Color.Black;
+            this.buttonOpenLobbyConfig.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.buttonOpenLobbyConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpenLobbyConfig.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonOpenLobbyConfig.Location = new System.Drawing.Point(271, 241);
+            this.buttonOpenLobbyConfig.Name = "buttonOpenLobbyConfig";
+            this.buttonOpenLobbyConfig.Size = new System.Drawing.Size(283, 26);
+            this.buttonOpenLobbyConfig.TabIndex = 20;
+            this.buttonOpenLobbyConfig.Text = "OPEN LOBBY SERVER CONFIG";
+            this.buttonOpenLobbyConfig.UseVisualStyleBackColor = false;
+            this.buttonOpenLobbyConfig.Click += new System.EventHandler(this.buttonOpenLobbyConfig_Click);
+            // 
+            // textBoxNetport
+            // 
+            this.textBoxNetport.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBoxNetport.Font = new System.Drawing.Font("BankGothic Lt BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNetport.Location = new System.Drawing.Point(446, 163);
+            this.textBoxNetport.MaxLength = 5;
+            this.textBoxNetport.Name = "textBoxNetport";
+            this.textBoxNetport.Size = new System.Drawing.Size(107, 21);
+            this.textBoxNetport.TabIndex = 19;
+            this.textBoxNetport.Text = "28960";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(355, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 14);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "LAN MODE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(360, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 14);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "NET PORT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 14);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "GAME MODE";
+            // 
+            // comboBoxPlaylist
+            // 
+            this.comboBoxPlaylist.Font = new System.Drawing.Font("BankGothic Lt BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxPlaylist.FormattingEnabled = true;
+            this.comboBoxPlaylist.Location = new System.Drawing.Point(116, 193);
+            this.comboBoxPlaylist.Name = "comboBoxPlaylist";
+            this.comboBoxPlaylist.Size = new System.Drawing.Size(233, 21);
+            this.comboBoxPlaylist.TabIndex = 11;
+            // 
+            // comboBoxLAN
+            // 
+            this.comboBoxLAN.Font = new System.Drawing.Font("BankGothic Lt BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxLAN.FormattingEnabled = true;
+            this.comboBoxLAN.Items.AddRange(new object[] {
+            "LAN",
+            "ONLINE"});
+            this.comboBoxLAN.Location = new System.Drawing.Point(446, 193);
+            this.comboBoxLAN.Name = "comboBoxLAN";
+            this.comboBoxLAN.Size = new System.Drawing.Size(108, 21);
+            this.comboBoxLAN.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 14);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "MOD";
+            // 
+            // comboBoxMod
+            // 
+            this.comboBoxMod.Font = new System.Drawing.Font("BankGothic Lt BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxMod.FormattingEnabled = true;
+            this.comboBoxMod.Items.AddRange(new object[] {
+            "none"});
+            this.comboBoxMod.Location = new System.Drawing.Point(116, 162);
+            this.comboBoxMod.Name = "comboBoxMod";
+            this.comboBoxMod.Size = new System.Drawing.Size(233, 21);
+            this.comboBoxMod.TabIndex = 7;
+            // 
+            // buttonDedicatedServer
+            // 
+            this.buttonDedicatedServer.BackColor = System.Drawing.Color.Black;
+            this.buttonDedicatedServer.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonDedicatedServer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDedicatedServer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDedicatedServer.Location = new System.Drawing.Point(11, 273);
+            this.buttonDedicatedServer.Name = "buttonDedicatedServer";
+            this.buttonDedicatedServer.Size = new System.Drawing.Size(254, 41);
+            this.buttonDedicatedServer.TabIndex = 6;
+            this.buttonDedicatedServer.Text = "START DEDICATED SERVER";
+            this.buttonDedicatedServer.UseVisualStyleBackColor = false;
+            this.buttonDedicatedServer.Click += new System.EventHandler(this.buttonDedicatedServer_Click);
+            // 
+            // buttonPartyServer
+            // 
+            this.buttonPartyServer.BackColor = System.Drawing.Color.Black;
+            this.buttonPartyServer.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonPartyServer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPartyServer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonPartyServer.Location = new System.Drawing.Point(271, 273);
+            this.buttonPartyServer.Name = "buttonPartyServer";
+            this.buttonPartyServer.Size = new System.Drawing.Size(283, 41);
+            this.buttonPartyServer.TabIndex = 5;
+            this.buttonPartyServer.Text = "START DEDICATED PARTY SERVER";
+            this.buttonPartyServer.UseVisualStyleBackColor = false;
+            this.buttonPartyServer.Click += new System.EventHandler(this.buttonPartyServer_Click);
+            // 
+            // buttonMultiplayer
+            // 
+            this.buttonMultiplayer.BackColor = System.Drawing.Color.Black;
+            this.buttonMultiplayer.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonMultiplayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMultiplayer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonMultiplayer.Location = new System.Drawing.Point(175, 11);
+            this.buttonMultiplayer.Name = "buttonMultiplayer";
+            this.buttonMultiplayer.Size = new System.Drawing.Size(158, 43);
+            this.buttonMultiplayer.TabIndex = 3;
+            this.buttonMultiplayer.Text = "MULTIPLAYER";
+            this.buttonMultiplayer.UseVisualStyleBackColor = false;
+            this.buttonMultiplayer.Click += new System.EventHandler(this.buttonMultiplayer_Click);
+            // 
+            // buttonSingleplayer
+            // 
+            this.buttonSingleplayer.BackColor = System.Drawing.Color.Black;
+            this.buttonSingleplayer.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonSingleplayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSingleplayer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSingleplayer.Location = new System.Drawing.Point(11, 11);
+            this.buttonSingleplayer.Name = "buttonSingleplayer";
+            this.buttonSingleplayer.Size = new System.Drawing.Size(158, 43);
+            this.buttonSingleplayer.TabIndex = 2;
+            this.buttonSingleplayer.Text = "SINGLEPLAYER";
+            this.buttonSingleplayer.UseVisualStyleBackColor = false;
+            this.buttonSingleplayer.Click += new System.EventHandler(this.buttonSingleplayer_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(524, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 327);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IW4x Launcher";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonDedicatedServer;
+        private System.Windows.Forms.Button buttonPartyServer;
+        private System.Windows.Forms.Button buttonMultiplayer;
+        private System.Windows.Forms.Button buttonSingleplayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxMod;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxPlaylist;
+        private System.Windows.Forms.ComboBox comboBoxLAN;
+        private System.Windows.Forms.TextBox textBoxNetport;
+        private System.Windows.Forms.Button buttonOpenServerConfig;
+        private System.Windows.Forms.Button buttonOpenLobbyConfig;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace IW4x_Server_Launcher
+{
+    public partial class Form1 : Form
+    {
+        private string arg_fs_game          = "none";
+        private string arg_sv_lanonly       = "1";
+        private string arg_net_port         = "28960";
+        private string arg_party_enable     = "0";
+        private string arg_playlist         = "0";
+        private string arg_playlistFilename = "playlists_launcher.info";
+        private string arg_serverFilename   = "server.cfg";
+
+        private string argument_string_party = "";
+        private string argument_string_dedicated = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            get_mods();
+            get_gamemodes();
+            
+            get_saved_details();
+
+            comboBoxLAN.DropDownStyle      = ComboBoxStyle.DropDownList;
+            comboBoxMod.DropDownStyle      = ComboBoxStyle.DropDownList;
+            comboBoxPlaylist.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        // Click anywhere to move hack
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+
+        private void buttonSingleplayer_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("iw4sp.exe")) return;
+
+            Process.Start("iw4sp.exe");
+            Environment.Exit(0);
+        }
+
+        private void buttonMultiplayer_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("iw4x.exe")) return;
+
+            update_arguments();
+            save_details();
+
+
+            if (comboBoxMod.SelectedItem.ToString() != "none")
+            {
+                arg_fs_game = "mods/" + comboBoxMod.SelectedItem.ToString();
+            }
+            else
+            {
+                arg_fs_game = "";
+            }
+
+            var p = new Process();
+            p.StartInfo.FileName = Path.GetFullPath("iw4x.exe");
+            p.StartInfo.Arguments = "+set fs_game \"" + arg_fs_game + "\"";
+            p.Start();
+            Environment.Exit(0);
+
+        }
+
+        private void buttonDedicatedServer_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("iw4x.exe")) return;
+
+            arg_party_enable = "0";
+            update_arguments();
+            save_details();
+
+            var p = new Process();
+            p.StartInfo.FileName = Path.GetFullPath("iw4x.exe");
+            p.StartInfo.Arguments = argument_string_dedicated;
+            p.Start();
+        }
+
+        private void buttonPartyServer_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("iw4x.exe")) return;
+
+            arg_party_enable = "1";
+            arg_serverFilename = "partyserver.cfg";
+            update_arguments();
+            save_details();
+
+            var p = new Process();
+            p.StartInfo.FileName = Path.GetFullPath("iw4x.exe");
+            p.StartInfo.Arguments = argument_string_party;
+            p.Start();
+        }
+
+        public void update_arguments()
+        {
+            arg_net_port = textBoxNetport.Text;
+            string playlist_id = "1";
+
+            foreach (Match id in Regex.Matches(comboBoxPlaylist.SelectedItem.ToString(), @"\[(.*?)\]"))
+            {
+                playlist_id = id.Groups[1].ToString();
+
+            }
+
+            // Playlists are always -1 of the selected value for some reason..
+            if(int.Parse(playlist_id) > 0)
+            {
+                int int_playlist_id = int.Parse(playlist_id);
+                int_playlist_id = int_playlist_id - 1;
+                playlist_id = int_playlist_id.ToString();
+            }
+
+            arg_playlist = playlist_id;
+
+            if (comboBoxMod.SelectedItem.ToString() != "none")
+            {
+                arg_fs_game = "mods/"+ comboBoxMod.SelectedItem.ToString();
+            }
+            else
+            {
+                arg_fs_game = "";
+            }
+
+            if (comboBoxLAN.SelectedItem.ToString().ToUpper() == "LAN") arg_sv_lanonly = "1";
+            if (comboBoxLAN.SelectedItem.ToString().ToUpper() == "ONLINE")  arg_sv_lanonly = "0";
+
+
+            argument_string_dedicated = "-dedicated +set fs_game \""+ arg_fs_game +
+                                   "\" +set sv_lanonly \""+ arg_sv_lanonly +
+                                   "\" +set net_port \"" + arg_net_port +
+                                   "\" +exec \"" + arg_serverFilename +
+                                   "\" +set party_enable \"" + arg_party_enable +
+            "\"";
+
+            argument_string_party = "-dedicated +set fs_game \"" + arg_fs_game +
+                       "\" +set sv_lanonly \"" + arg_sv_lanonly +
+                       "\" +set net_port \"" + arg_net_port +
+                       "\" +exec \"" + arg_serverFilename +
+                       "\" +set party_enable \"" + arg_party_enable +
+                       "\" +playlist \"" + arg_playlist +
+                       "\" +set playlistFilename \"" + arg_playlistFilename + "\"";
+
+        }
+
+        public void get_mods()
+        {
+            if(Directory.Exists("mods") && !IsDirectoryEmpty("mods"))
+            {
+                foreach(string modfolder in Directory.GetDirectories("mods"))
+                {
+                    comboBoxMod.Items.Add(Path.GetFileName(modfolder));
+                }
+            }
+        }
+
+        public bool IsDirectoryEmpty(string path)
+        {
+            IEnumerable<string> items = Directory.EnumerateFileSystemEntries(path);
+            using (IEnumerator<string> en = items.GetEnumerator())
+            {
+                return !en.MoveNext();
+            }
+        }
+
+        private void buttonOpenServerConfig_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(@"userraw\server.cfg"))
+            {
+                MessageBox.Show(@"userraw\server.cfg does not exist, please create the file before trying to open it with this launcher");
+                return;
+            }
+
+            Process.Start(@"userraw\server.cfg");
+        }
+
+        private void buttonOpenLobbyConfig_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(@"userraw\partyserver.cfg"))
+            {
+                MessageBox.Show(@"userraw\partyserver.cfg does not exist, please create the file before trying to open it with this launcher");
+                return;
+            }
+            
+            Process.Start(@"userraw\partyserver.cfg");
+        }
+
+        private void get_gamemodes()
+        {
+            var ConfigFile = Path.GetFullPath("launcher.ini");
+            arg_playlist = IniFile.ReadValue(ConfigFile, "launcher", "playlist");
+
+            if (!File.Exists("userraw\\playlists_launcher.info"))
+            {
+                label4.Visible = false;
+                comboBoxPlaylist.Visible = false;
+                buttonPartyServer.Visible = false;
+                buttonOpenLobbyConfig.Visible = false;
+                return;
+            }
+
+
+            string[] playlist_lines = File.ReadAllLines("userraw\\playlists_launcher.info");
+            comboBoxPlaylist.Items.Clear();
+            int i = 1;
+            string last_line = "";
+            foreach (string line in playlist_lines)
+            {
+                if (last_line.Length > 9 && last_line.Substring(0, 9) == "playlist ")
+                {
+                    var playlist_value = last_line.Split(' ', '	').Skip(1).FirstOrDefault();
+                    var playlist_string = "[" + playlist_value + "] " + line.Remove(0, 13).Replace("\"", "").ToUpper();
+
+                    comboBoxPlaylist.Items.Add(playlist_string);
+                    if (playlist_value == arg_playlist)
+                    {
+                        comboBoxPlaylist.SelectedItem = playlist_string;
+                    }
+                        
+                }
+
+                i++;
+                last_line = line;
+            }
+
+
+        }
+
+        public void get_saved_details()
+        {
+            var ConfigFile = Path.GetFullPath("launcher.ini");
+
+            arg_fs_game = IniFile.ReadValue(ConfigFile, "launcher", "mod");
+            comboBoxMod.SelectedItem = arg_fs_game;
+            if (arg_fs_game.Length == 0)
+                comboBoxMod.SelectedIndex = 0;
+
+            arg_sv_lanonly = IniFile.ReadValue(ConfigFile, "launcher", "lanmode");
+            comboBoxLAN.SelectedIndex = 1;
+            if (arg_sv_lanonly == "1")
+                comboBoxLAN.SelectedItem = "LAN";
+
+            arg_net_port = IniFile.ReadValue(ConfigFile, "launcher", "netport");
+            if (arg_net_port.Length == 0)
+                arg_net_port = "28960";
+
+            textBoxNetport.Text = arg_net_port;
+
+        }
+
+        public void save_details()
+        {
+            var ConfigFile = Path.GetFullPath("launcher.ini");
+
+            foreach (Match id in Regex.Matches(comboBoxPlaylist.SelectedItem.ToString(), @"\[(.*?)\]"))
+            {
+                var playlist_id = id.Groups[1].ToString();
+                IniFile.WriteValue(ConfigFile, "launcher", "playlist", playlist_id);
+            }
+
+            string mod = comboBoxMod.SelectedItem.ToString();
+            if (arg_fs_game.Contains("mods/")) mod = arg_fs_game.Substring(5);
+
+            IniFile.WriteValue(ConfigFile, "launcher", "mod", mod);
+            IniFile.WriteValue(ConfigFile, "launcher", "lanmode", arg_sv_lanonly);
+            IniFile.WriteValue(ConfigFile, "launcher", "netport", arg_net_port);
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
